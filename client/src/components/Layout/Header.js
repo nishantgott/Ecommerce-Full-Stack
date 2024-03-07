744e639ec06dfad5150e6380f46e13bc7d8ee0d1
nishantgott <nishantgk2004@gmail.com>
2024-03-07 12:25:23 +0530

    added dropdown in header

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -40,10 +40,32 @@ const Header = () => {
                             {
                                 (auth.user) ? (
                                     <>
-                                        <li className="nav-item">
-                                            <NavLink onClick={handleLogout} to="/login" className="nav-link">
-                                                Logout
+                                        <li className="nav-item dropdown">
+                                            <NavLink
+                                                className="nav-link dropdown-toggle"
+                                                href="#"
+                                                role="button"
+                                                data-bs-toggle="dropdown"
+                                                aria-expanded="false"
+                                            >
+                                                {auth?.user?.name}
                                             </NavLink>
+                                            <ul className="dropdown-menu">
+                                                <li>
+                                                    <NavLink to="/dashboard" className="dropdown-item">
+                                                        Dashboard
+                                                    </NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink
+                                                        onClick={handleLogout}
+                                                        to="/login"
+                                                        className="dropdown-item"
+                                                    >
+                                                        Logout
+                                                    </NavLink>
+                                                </li>
+                                            </ul>
                                         </li>
                                     </>
                                 )
